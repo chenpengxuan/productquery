c586b67d97c1ea0081a6b8acfbf5a6282261fdeb
chenpengxuan <18602201@qq.com>
2017-04-18 18:49:20 +0800

    解决版本冲突问题

--- test/src/test/java/com/ymatou/productquery/test/domain/MongoRepositoryTest.java
+++ test/src/test/java/com/ymatou/productquery/test/domain/MongoRepositoryTest.java
@@ -5,12 +5,10 @@ import com.ymatou.productquery.domain.repo.mongorepo.ProductRepository;
 import com.ymatou.productquery.web.ProductQueryApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,29 +19,11 @@ import java.util.List;
 @SpringBootTest(classes = ProductQueryApplication.class)// 指定我们SpringBoot工程的Application启动类
 public class MongoRepositoryTest {
 
-//    @Resource(name = "mongoRepo")
-//    private Repository mongoRepository;
+    @Resource(name = "mongoRepo")
+    private ProductRepository mongoRepository;
 
     @Resource
     private ProductRepository productRepository;
-
-//    @Test
-//    public void testGetActivityProductList() {
-//        List<String> productids = new ArrayList<>();
-//        productids.add("4b37659f-fb97-4a81-8ba6-a6f884a2caa0");
-//        productids.add("2baf8ca7-34e3-405b-ae54-7cad82430554");
-//        productids.add("c69abee5-2beb-4e73-b2ea-5236179319de");
-//        List<ActivityProducts> aps = mongoRepository.getActivityProductList(productids);
-//    }
-//
-//    @Test
-//    public void testGetHistoryProductListByProductIdList() {
-//        List<String> productids = new ArrayList<>();
-//        productids.add("000005c0-8798-4d50-a713-13cbee9c5b5b");
-//        productids.add("0000129c-2fac-42ea-8707-4c7acecb34ba");
-//        productids.add("000012ba-2db5-4f1d-a9a3-ab28d1a8fc41");
-//        List<ProductDetailModel> pds = mongoRepository.getHistoryProductListByProductIdList(productids);
-//    }
 
     @Test
     public void testGetProductsByProductIds() {
--- test/src/test/java/com/ymatou/productquery/test/facade/GetCatalogListByCatalogIdListTest.java
+++ test/src/test/java/com/ymatou/productquery/test/facade/GetCatalogListByCatalogIdListTest.java
@@ -1,20 +1,11 @@
 package com.ymatou.productquery.test.facade;
 
 import com.ymatou.productquery.facade.ProductQueryFacade;
-import com.ymatou.productquery.model.req.GetCatalogListByCatalogIdListRequest;
-import com.ymatou.productquery.model.res.BaseResponseNetAdapter;
 import com.ymatou.productquery.web.ProductQueryApplication;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by zhangyong on 2017/4/13.
@@ -25,29 +16,29 @@ public class GetCatalogListByCatalogIdListTest {
     @Autowired
     private ProductQueryFacade productQueryFacade;
 
-    @Test
-    public void testWithoutActivityOrLive() {
-        List<String> catalogs = new ArrayList<>();
-        catalogs.add("e5f78f4a-4f45-4bf1-8662-65eb8f8ccbd9");
-        catalogs.add("58aa4923-c05f-45f4-b9ee-dfb409eb709c");
-        catalogs.add("673db9d9-1e5e-447e-b7df-935bf7d4c47a");
-
-        GetCatalogListByCatalogIdListRequest request = new GetCatalogListByCatalogIdListRequest();
-        request.setCatalogIdList(catalogs);
-        BaseResponseNetAdapter response = productQueryFacade.getCatalogListByCatalogIdList(request);
-        assertEquals(200, response.getCode());
-    }
-
-    @Test
-    public void testWithActivity() {
-        List<String> catalogs = new ArrayList<>();
-        catalogs.add("1996c44c-c305-4682-bce4-0bb4bd9b9149");
-        catalogs.add("9c24c428-3cab-45eb-9b6a-2dc914914097");
-        catalogs.add("f5f2729d-0a77-4430-b82e-bbb02c6b7d71");
-
-        GetCatalogListByCatalogIdListRequest request = new GetCatalogListByCatalogIdListRequest();
-        request.setCatalogIdList(catalogs);
-        BaseResponseNetAdapter response = productQueryFacade.getCatalogListByCatalogIdList(request);
-        assertEquals(200, response.getCode());
-    }
+//    @Test
+//    public void testWithoutActivityOrLive() {
+//        List<String> catalogs = new ArrayList<>();
+//        catalogs.add("e5f78f4a-4f45-4bf1-8662-65eb8f8ccbd9");
+//        catalogs.add("58aa4923-c05f-45f4-b9ee-dfb409eb709c");
+//        catalogs.add("673db9d9-1e5e-447e-b7df-935bf7d4c47a");
+//
+//        GetCatalogListByCatalogIdListRequest request = new GetCatalogListByCatalogIdListRequest();
+//        request.setCatalogIdList(catalogs);
+//        BaseResponseNetAdapter response = productQueryFacade.getCatalogListByCatalogIdList(request);
+//        assertEquals(200, response.getCode());
+//    }
+//
+//    @Test
+//    public void testWithActivity() {
+//        List<String> catalogs = new ArrayList<>();
+//        catalogs.add("1996c44c-c305-4682-bce4-0bb4bd9b9149");
+//        catalogs.add("9c24c428-3cab-45eb-9b6a-2dc914914097");
+//        catalogs.add("f5f2729d-0a77-4430-b82e-bbb02c6b7d71");
+//
+//        GetCatalogListByCatalogIdListRequest request = new GetCatalogListByCatalogIdListRequest();
+//        request.setCatalogIdList(catalogs);
+//        BaseResponseNetAdapter response = productQueryFacade.getCatalogListByCatalogIdList(request);
+//        assertEquals(200, response.getCode());
+//    }
 }
