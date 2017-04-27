a4193e294e2e5c135ed7e2629d752f3785d68a84
chenpengxuan <18602201@qq.com>
2017-04-27 17:08:58 +0800

    增加数据交互接口

---
+++ domain/src/main/java/com/ymatou/productquery/domain/service/CommonQueryService.java
@@ -0,0 +1,59 @@
+package com.ymatou.productquery.domain.service;
+
+import com.ymatou.productquery.domain.model.ActivityProducts;
+import com.ymatou.productquery.domain.model.Catalogs;
+import com.ymatou.productquery.domain.model.LiveProducts;
+import com.ymatou.productquery.domain.model.Products;
+
+import java.util.List;
+
+/**
+ * 列表查询与单品查询共用
+ * Created by chenpengxuan on 2017/4/26.
+ */
+public class CommonQueryService {
+    /**
+     * 根据商品id列表获取商品信息
+     * @param productIdList
+     * @return
+     */
+    List<Products> getProductListByProductIdList(List<String> productIdList){
+        return null;
+    }
+
+    /**
+     * 根据商品id获取商品信息
+     * @param productId
+     * @return
+     */
+    Products getProductByProductId(String productId){
+        return null;
+    }
+
+    /**
+     * 根据规格id列表获取规格信息
+     * @param catalogIdList
+     * @return
+     */
+    List<Catalogs> getCatalogListByCatalogIdList(List<String> catalogIdList){
+        return null;
+    }
+
+    /**
+     * 根据商品id列表获取活动商品信息列表
+     * @param productIdList
+     * @return
+     */
+    List<ActivityProducts> getActivityProductListByProductIdList(List<String> productIdList){
+        return null;
+    }
+
+    /**
+     * 根据直播商品id获取直播商品信息
+     * @param productId
+     * @return
+     */
+    LiveProducts getLiveProductByProductId(String productId){
+        return null;
+    }
+}
