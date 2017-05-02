2a8558092520a3a1dafd5f5e27b6f87cfbb9bae8
chenpengxuan <18602201@qq.com>
2017-05-02 11:07:10 +0800

    缓存功能 数据操作功能

--- domain/src/main/java/com/ymatou/productquery/domain/model/cache/CacheActivityProductInfo.java
+++ domain/src/main/java/com/ymatou/productquery/domain/model/cache/CacheActivityProductInfo.java
@@ -1,6 +1,8 @@
 package com.ymatou.productquery.domain.model.cache;
 
 import com.ymatou.productquery.domain.model.ActivityCatalogInfo;
+import com.ymatou.productquery.domain.model.ActivityProducts;
+import com.ymatou.productquery.infrastructure.util.Utils;
 
 import java.util.Date;
 import java.util.List;
@@ -68,4 +70,110 @@ public class CacheActivityProductInfo extends BaseCacheInfo{
      *
      */
     private List<ActivityCatalogInfo> activityCatalogList;
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public int getProductInActivityId() {
+        return productInActivityId;
+    }
+
+    public void setProductInActivityId(int productInActivityId) {
+        this.productInActivityId = productInActivityId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Double getMarketPrice() {
+        return marketPrice;
+    }
+
+    public void setMarketPrice(Double marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+
+    public int getActivityLimit() {
+        return activityLimit;
+    }
+
+    public void setActivityLimit(int activityLimit) {
+        this.activityLimit = activityLimit;
+    }
+
+    public int getProductLimit() {
+        return productLimit;
+    }
+
+    public void setProductLimit(int productLimit) {
+        this.productLimit = productLimit;
+    }
+
+    public boolean isTradeIsolation() {
+        return isTradeIsolation;
+    }
+
+    public void setTradeIsolation(boolean tradeIsolation) {
+        isTradeIsolation = tradeIsolation;
+    }
+
+    public boolean isNewBuyer() {
+        return isNewBuyer;
+    }
+
+    public void setNewBuyer(boolean newBuyer) {
+        isNewBuyer = newBuyer;
+    }
+
+    public boolean isPartCatalogs() {
+        return isPartCatalogs;
+    }
+
+    public void setPartCatalogs(boolean partCatalogs) {
+        isPartCatalogs = partCatalogs;
+    }
+
+    public List<ActivityCatalogInfo> getActivityCatalogList() {
+        return activityCatalogList;
+    }
+
+    public void setActivityCatalogList(List<ActivityCatalogInfo> activityCatalogList) {
+        this.activityCatalogList = activityCatalogList;
+    }
+
+    /**
+     * 数据转换器
+     * @return
+     */
+    public ActivityProducts convertCacheDataToDto(){
+        ActivityProducts activityProducts = new ActivityProducts();
+        Utils.copyProperties(activityProducts,this);
+        return activityProducts;
+    }
 }
